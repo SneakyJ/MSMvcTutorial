2fc0a8710adedd0768578f14ebaf7fa5c4a69485
SneakyJ <k.nikolaienko@gmail.com>
2022-06-27 08:30:09 +0200

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome
+        public IActionResult Welcome(string name, int numTimes = 1) // Method(string name, int ID = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+            //return HtmlEncoder.Default.Encode(value: $"Hello {name}, ID: {ID}"); //HtmlEncoder.Default.Encode to protect the app from malicious input, such as through JavaScript.
+        }
+    }
+}
